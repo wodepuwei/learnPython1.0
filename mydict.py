3032f5fe5f779af2f621c05ce30a82d2e848fe1b
wodepuwei <xue0808@126.com>
2019-06-27 22:00:46 +0800

    backup 2019年 6月27日 星期四 22时00分46秒 CST

--- mydict.py
+++ mydict.py
@@ -1,12 +1,32 @@
 #!/usr/bin/python 
 #__*__coding:utf8__*__ 
 
-import unittest
-
 class Dict(dict):
+	'''
+    Simple dict but also support access as x.y style.
+
+    >>> d1 = Dict()
+    >>> d1['x'] = 100
+    >>> d1.x
+    100
+    >>> d1.y = 200
+    >>> d1['y']
+    200
+    >>> d2 = Dict(a=1, b=2, c='3')
+    >>> d2.c
+    '3'
+    >>> d2['empty']
+    Traceback (most recent call last):
+        ...
+    KeyError: 'empty'
+    >>> d2.empty
+    Traceback (most recent call last):
+        ...
+    AttributeError: 'Dict' object has no attribute 'empty'
+	'''
+
 	def __init__(self,**kw):
 		super(Dict,self).__init__(**kw)
-
 	def __getattr__(self,key):
 		try:
 			return self[key]
@@ -15,41 +35,6 @@ class Dict(dict):
 	def __setattr__(self,key,value):
 		self[key]=value
 
-class TestDict(unittest.TestCase):
-	def setUp(self):
-		print 'setUp...'
-
-	def tearDown(self):
-		print 'tearDown...'	
-
-	def test_init(self):
-		d = Dict(a=1,b='c')
-		self.assertEquals(d.a,1)
-		self.assertEquals(d.b,'c')
-		self.assertTrue(isinstance(d,dict))
-
-	def test_key(self):
-		d = Dict() 
-		d['key'] = 'value'
-		self.assertEquals(d.key,'value')
-
-	def test_attr(self):
-		d = Dict()
-		d.key = 'value'
-		self.assertTrue('key' in d)
-		self.assertEquals(d['key'],'value')
-
-	def test_keyerror(self):
-		d = Dict() 
-		with self.assertRaises(KeyError):
-			value = d['empty']
-
-	def test_attrerror(self):
-		d = Dict() 
-		with self.assertRaises(AttributeError):
-			value = d.empty
-
-
-
 if __name__=='__main__':
-	unittest.main()
+	import doctest
+	doctest.testmod(verbose=True)
